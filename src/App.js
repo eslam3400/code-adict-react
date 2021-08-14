af9b5b69824e59f9fcf3fa62cd4036a7dee4f8f5
Eslam Magdy <eslam3400@gmail.com>
2021-08-14 12:57:38 +0200

    fix ui and add username functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import AddTodo from './components/add-todo/AddTodo';
 import BottomNavbar from './components/bottomNavbar/BottomNavbar';
@@ -9,16 +9,27 @@ import TodoList from './components/todo-list/TodoList';
 export default function App() {
   const [showAddingNewTaskInput, setShowAddingNewTaskInput] = useState(false)
   const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || [])
+  const [user, setUser] = useState(localStorage.getItem("user"))
+
+  useEffect(() => {
+    if (!user) {
+      let username = prompt("Please Enter Your Name");
+      localStorage.setItem("user", username);
+      setUser(username);
+    }
+  }, [])
 
   return (
     <main>
-      <div className="appBody" onClick={e => setShowAddingNewTaskInput(false)}>
-        {showAddingNewTaskInput ? <FadedBackground /> : null}
-        <Header />
-        <TodoList tasks={tasks} setTasks={setTasks} />
-      </div>
-      <div className="BottomNavbar" style={{ backgroundColor: showAddingNewTaskInput ? "white" : 'black' }}>
-        {showAddingNewTaskInput ? <AddTodo setTasks={setTasks} showInputHandler={setShowAddingNewTaskInput} /> : <BottomNavbar adding={showAddingNewTaskInput} addingHandler={setShowAddingNewTaskInput} />}
+      <div className="appContainer">
+        <div className="appBody" onClick={e => setShowAddingNewTaskInput(false)}>
+          {showAddingNewTaskInput ? <FadedBackground /> : null}
+          <Header />
+          <TodoList tasks={tasks} setTasks={setTasks} />
+        </div>
+        <div className="BottomNavbar" style={{ backgroundColor: showAddingNewTaskInput ? "white" : 'black' }}>
+          {showAddingNewTaskInput ? <AddTodo setTasks={setTasks} showInputHandler={setShowAddingNewTaskInput} /> : <BottomNavbar adding={showAddingNewTaskInput} addingHandler={setShowAddingNewTaskInput} />}
+        </div>
       </div>
     </main>
   )
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import './Header.css'
 
 export default function Header() {
+
+  const getUsernameFirstLetter = ()=> localStorage.getItem("user")? localStorage.getItem("user")[0].toLocaleUpperCase():""
+
   return (
     <section className="header">
       <div className="header__left">
         <h2>My Tasks</h2>
       </div>
       <div className="header__right">
-        E
+        {getUsernameFirstLetter()}
       </div>
     </section>
   )
